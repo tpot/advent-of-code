9d3f0d595f2eea1ad302988876e39d2f00a7ae33
Tim Potter <tpot@frungy.org>
2023-12-03 08:07:41 +1100

    Solution for part 2, day 5, 2015

--- src/year2015/day05/core.clj
+++ src/year2015/day05/core.clj
@@ -2,6 +2,10 @@
   (:require [clojure.string :as s]
             [clojure.test :refer [deftest is]]))
 
+(def puzzle-input (-> (slurp "resources/year2015/day05/input") s/split-lines))
+
+;; Part 1
+
 (defn three-vowels?
   "Return true if string contains at least three vowels."
   [x]
@@ -41,31 +45,70 @@
   (is (contains-naughty-substrings? "haegwjzuvuyypxyu"))
   (is (not (contains-naughty-substrings? "qqqq"))))
 
-(defn is-nice?
+(defn is-nice-part1?
   "Return true if string satifies all conditions for being nice."
   [x]
   (and (three-vowels? x)
        (doubles? x)
        (not (contains-naughty-substrings? x))))
 
-(deftest test-is-nice?
+(deftest test-is-nice-part1?
   (let [test-input {"ugknbfddgicrmopn" true
                     "aaa"              true
                     "jchzalrnumimnmhp" false
                     "haegwjzuvuyypxyu" false
                     "dvszwmarrgswjxmb" false}]
-    (is (= (map is-nice? (keys test-input))
+    (is (= (map is-nice-part1? (keys test-input))
            (vals test-input)))))
-
-;; Part 1
 
 (comment
 
-  (def puzzle-input (-> (slurp "resources/year2015/day05/input") s/split-lines))
-
   (->> puzzle-input
-       (map is-nice?)
+       (map is-nice-part1?)
        (filter true?)
        count) ;; => 255
+
+  :rcf)
+
+;; Part 2
+
+(defn contains-nonoverlapping-pair?
+  "Does the string contain a pair of characters that do not overlap?"
+  [s]
+  (let [pairs         (partition 2 1 s)
+        indexed-pairs (map-indexed #(hash-map :index %1 :pair %2) pairs)
+        grouped-pairs (group-by :pair indexed-pairs)]
+    (reduce
+     (fn [_ input]
+       (let [indexes (map :index input)]
+         (if (->> (for [i indexes j indexes] (vector i j))
+                  (map #(abs (- (first %) (second %))))
+                  (some #(> % 1)))
+           (reduced true)
+           false)))
+     false
+     (vals grouped-pairs))))
+
+(deftest test-contains-nonoverlapping-pair?
+  (is (contains-nonoverlapping-pair? "xxyxx"))
+  (is (not (contains-nonoverlapping-pair? "aaa")))
+  (is (contains-nonoverlapping-pair? "aaaa")))
+
+(defn contains-duplicate-with-inbetween?
+  [s]
+  (->> (partition 3 1 s)
+       (some (fn [[a _ c]] (= a c)))))
+
+(defn is-nice-part2?
+  [s]
+  (and (contains-nonoverlapping-pair? s)
+       (contains-duplicate-with-inbetween? s)))
+
+(comment
+
+  (->> puzzle-input
+       (map is-nice-part2?)
+       (filter true?)
+       count) ;; => 55
 
   :rcf)
