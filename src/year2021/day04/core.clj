970a2dbce5cb87ebdf8ed7703932ab6359ffebfe
Tim Potter <tpot@frungy.org>
2024-11-27 06:29:05 +1100

    Solution for part 1, day 4, 2021

---
+++ src/year2021/day04/core.clj
@@ -0,0 +1,103 @@
+(ns year2021.day04.core
+  (:require [clojure.string :as s]
+            [clojure.set :as set]
+            [clojure.pprint :refer [pprint]]
+            [clojure.test :refer [deftest is run-tests]]))
+
+;; Input parsing
+
+(def test-input "resources/year2021/day04/test-input")
+(def puzzle-input "resources/year2021/day04/input")
+
+(defn lines->board
+  "Convert a seq of newline-separated strings of space-separated integers
+   into a 2-dimensional seq of integers."
+  [lines]
+  (->> lines
+       (map s/trim)
+       (map #(s/split % #"\s+"))
+       (map #(map parse-long %))
+       (map vec)))
+
+(defn parse-input
+  [input]
+  (let [lines (-> (slurp input) s/split-lines)
+        draw-line (get lines 0)
+        board-lines (->> (partition-by count (drop 2 lines))
+                         (remove #(= (count %) 1)))]
+    {:draw (vec (map parse-long (s/split draw-line #",")))
+     :boards (map lines->board board-lines)}))
+
+;; Helpers
+
+(defn board-wins?
+  "Return the board if any subset of numbers in the draw are equal to a
+   row or column of the board, nil otherwise."
+  [board draw]
+  (let [row-groups (map set board)
+        col-groups (map set (apply map vector board))]
+    (when
+     (some #(set/subset? % (set draw)) (concat row-groups col-groups))
+      board)))
+
+(deftest test-board-wins?
+  (let [board [[1 2] [3 4]]]
+    (is (some? (board-wins? board [1 2])))
+    (is (some? (board-wins? board [1 4 2])))
+    (is (nil? (board-wins? board [1 4])))
+    (is (nil? (board-wins? board [2 3])))))
+
+(defn draw-seq
+  "A sequence of draws representing the bingo moves."
+  [draw]
+  (map #(take % draw) (range 1 (inc (count draw)))))
+
+(deftest test-draw-seq
+  (let [draw [1 2 3]]
+    (is (= [[1] [1 2] [1 2 3]] (draw-seq draw)))))
+
+(defn marked-numbers
+  "Return the marked numbers from draw on board"
+  [board draw]
+  (set/intersection (set draw) (set (mapcat vec board))))
+
+(deftest test-marked-numbers
+  (is (= (set [1 2 3]) (marked-numbers [[1 2 3] [4 5 6]] [1 2 0 3]))))
+
+(defn unmarked-numbers
+  "Return the unmarked numbers from draw on board"
+  [board draw]
+  (let [all (set (mapcat vec board))
+        marked (marked-numbers board draw)]
+    (set/difference all marked)))
+
+(deftest test-unmarked-numbers
+  (is (= (set [4 5 6]) (unmarked-numbers [[1 2 3] [4 5 6]] [1 2 0 3]))))
+
+;; Part 1
+
+(defn part01
+  [input]
+  (let [game (parse-input input)
+        winner (reduce
+                (fn [_ draw]
+                  (when-let [winner (some #(board-wins? % draw) (get game :boards))]
+                    (reduced {:board winner :draw draw})))
+                nil
+                (draw-seq (get game :draw)))]
+    (->> (unmarked-numbers (get winner :board) (get winner :draw))
+         (reduce + 0)
+         (* (last (get winner :draw))))))
+
+(deftest test-part01
+  (is (= 4512 (part01 test-input)))
+  (is (= 27027 (part01 puzzle-input))))
+
+;; Part 2
+
+(defn part02
+  [input])
+
+(deftest test-part02)
+
+(run-tests)
