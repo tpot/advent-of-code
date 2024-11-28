bde9a6eba3aac5d1d0ea73c1c5a78317eae0c5c8
Tim Potter <tpot@frungy.org>
2024-11-28 19:36:31 +1100

    Solution for part 2, day 6, 2021

--- src/year2021/day06/core.clj
+++ src/year2021/day06/core.clj
@@ -33,9 +33,32 @@
 
 ;; Part 2
 
-(defn part02
-  [input])
+(defn timer-seq->timer-count
+  "Convert seq of timer values to a vector of timer value counts"
+  [fish-timers]
+  (reduce
+  (fn [fish-timers [timer-val count]]
+    (update fish-timers timer-val + count))
+  (vec (take 9 (repeat 0)))
+  (-> (group-by identity fish-timers)
+      (update-vals count))))
 
-(deftest test-part02)
+(defn part02
+  [input generations]
+  (->> (reduce
+        (fn [tc _]
+          (let [num-new-fish (get tc 0)]
+            (-> (drop 1 tc) vec (conj 0)   ; Pop off mothers with zero timer
+                (update 6 + num-new-fish)  ; Mother timers set to 6
+                (update 8 + num-new-fish)  ; Baby timers set to 8
+                )))
+        (timer-seq->timer-count (parse-input input))
+        (range generations))
+       (apply +)))
+
+(deftest test-part02
+  (is (= 5934 (part02 test-input 80)))
+  (is (= 387413 (part02 puzzle-input 80)))
+  (is (= 1738377086345 (part02 puzzle-input 256))))
 
 (run-tests)
