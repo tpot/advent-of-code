b53190a39b4bfaabdbca28eea9d01d135e0520b2
Tim Potter <tpot@frungy.org>
2024-12-02 17:34:41 +1100

    Solution for part 2, day 2, 2024

--- src/year2024/day02/core.clj
+++ src/year2024/day02/core.clj
@@ -34,11 +34,39 @@
 
 (deftest test-part01
   (is (= 2 (part01 test-input)))
-  (is (= nil (part01 puzzle-input))))
+  (is (= 236 (part01 puzzle-input))))
+
+(defn drop-one
+  "Return a seq of reports where one element is dropped"
+  [report]
+  (for [n (range (count report))]
+    (concat (take n report)
+            (rest (drop n report)))))
+
+(defn safe-with-damper?
+  [report]
+  (if (safe? report) true
+      (> (->> (drop-one report)
+              (filter safe?)
+              count)
+         0)))
+
+(deftest test-safe-with-damper?
+  (is (true?  (safe-with-damper? [7 6 4 2 1])))
+  (is (false? (safe-with-damper? [1 2 7 8 9])))
+  (is (false? (safe-with-damper? [9 7 6 2 1])))
+  (is (true?  (safe-with-damper? [1 3 2 4 5])))
+  (is (true?  (safe-with-damper? [8 6 4 4 1])))
+  (is (true?  (safe-with-damper? [1 3 6 7 9]))))
 
 (defn part02
-  [input])
+  [input]
+  (->> (parse-input input)
+       (filter safe-with-damper?)
+       count))
 
-(deftest test-part02)
+(deftest test-part02
+  (is (= 4 (part02 test-input)))
+  (is (= 308 (part02 puzzle-input))))
 
 (run-tests)
